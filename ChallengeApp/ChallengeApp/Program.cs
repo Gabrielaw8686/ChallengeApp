65480124ea59b416cc399a5984ad31cf0a17c249
Gabrielaw8686 <g.bulsiewicz@interia.pl>
2024-01-15 20:50:50 +0100

    Zadanie  nr 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isWoman = true;
+int age = 29;
+string name = "Ewa";
+
+
+if (isWoman  == true && age < 30)
+{
+    Console.WriteLine("KOBIETA PONIŻEJ 30 LAT");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("EWA, LAT 30");
+}
+else if (isWoman == false && age < 18)
+{
+    Console.WriteLine("NIEPEŁNOLETNI MEZCZYZNA");
+}   
+else
+{
+    Console.WriteLine("KTOS INNY");
+}
